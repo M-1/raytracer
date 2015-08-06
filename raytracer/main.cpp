8be98cdb4e785d5b2aeb51d37fc291cf99ac3db5
a <a@a.local>
2015-08-06 23:46:25 +0200

    Initial Commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by a on 06/08/15.
+//  Copyright (c) 2015 Martin Mi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
