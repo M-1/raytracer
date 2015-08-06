8ac04569d9366295450090d6401b27ff81d7c943
a <a@a.local>
2015-08-07 00:15:45 +0200

    ppm image output

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -7,12 +7,32 @@
 //
 
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char * argv[])
-{
+//! Type for usage as dimensions/coordinates
+typedef std::uint_fast16_t dim_t;
 
-	// insert code here...
-	std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[]) {
+	// code adapted from http://rosettacode.org/wiki/Bitmap/Write_a_PPM_file#C
+	dim_t size_x = 320, size_y = 240;
+	std::string out_file = "out.ppm";
+	
+	std::ofstream out(out_file, std::ios::binary);
+	
+	// Print the header
+	out << "P6\n" << size_x << " " << size_y << "\n255\n";
+	
+	for (dim_t y = 0; y < size_y; ++y) {
+		for (dim_t x = 0; x < size_x; ++x) {
+			static char color[3];
+			color[0] = x % 256; // red
+			color[1] = y % 256; // green
+			color[2] = (x * y) % 256; // blue
+			out.write(&color[0], 3);
+		}
+	}
+	
+	out.close();
+	
     return 0;
 }
-
